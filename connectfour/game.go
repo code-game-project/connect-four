3ad7cf7eb5a8602e3d863783d5b34ec2772e5fd7
Julian Hofmann <julian.hofmann2805@gmail.com>
2023-01-04 13:05:53 +0100

    Implement win detection

--- connectfour/game.go
+++ connectfour/game.go
@@ -52,8 +52,11 @@ func (g *Game) onPlayerSocketConnected(player *cg.Player, socket *cg.GameSocket)
 	if g.yellowPlayer == nil || g.redPlayer == nil {
 		return
 	}
+	if player.SocketCount() == 1 {
+		return
+	}
 
-	g.cg.Send(StartEvent, StartEventData{
+	socket.Send(StartEvent, StartEventData{
 		Colors: map[string]Color{
 			g.yellowPlayer.Id: ColorYellow,
 			g.redPlayer.Id:    ColorRed,
@@ -165,7 +168,58 @@ func (g *Game) dropInColumn(color Color, column int) error {
 }
 
 func (g *Game) checkDone() bool {
+	for row := range g.grid {
+		for col := range g.grid[row] {
+			// horizontal
+			if g.checkLine(row, col, 0, 1) {
+				return true
+			}
+
+			// vertical
+			if g.checkLine(row, col, 1, 0) {
+				return true
+			}
+
+			// diagonals
+
+			// right, down
+			if g.checkLine(row, col, 1, 1) {
+				return true
+			}
+
+			// right, up
+			if g.checkLine(row, col, -1, 1) {
+				return true
+			}
+		}
+	}
+
 	return false
+}
+
+func (g *Game) checkLine(row, col, dr, dc int) bool {
+	color := g.grid[row][col].Color
+	if color == ColorNone {
+		return false
+	}
+
+	if row+3*dr < 0 || row+3*dr >= g.config.Height || col+3*dc < 0 || col+3*dc >= g.config.Width {
+		return false
+	}
+
+	if g.grid[row+1*dr][col+1*dc].Color != color || g.grid[row+2*dr][col+2*dc].Color != color || g.grid[row+3*dr][col+3*dc].Color != color {
+		return false
+	}
+
+	g.gameOver(g.grid[row][col], g.grid[row+1*dr][col+1*dc], g.grid[row+2*dr][col+2*dc], g.grid[row+3*dr][col+3*dc])
+	return true
+}
+
+func (g *Game) gameOver(cells ...Cell) {
+	g.cg.Send(GameOverEvent, GameOverEventData{
+		WinnerColor: g.currentTurn,
+		WinningLine: cells,
+	})
 }
 
 func (g *Game) handleCommand(origin *cg.Player, cmd cg.Command) {
